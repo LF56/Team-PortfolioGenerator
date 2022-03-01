237e3d388bd60ec6c71855a0cdac114ed57f6bdb
LF56 <leah.fox7@gmail.com>
2022-03-01 16:36:16 -0500

    added employee functions

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require('../lib/Employee');
+
+test('create employee object', () => {
+    const name = 'Kate';
+    const id = '4739';
+    const email = 'kate@gmail.com';
+    const employee = new Employee(name,id,email);
+
+    expect(employee.name).toBe(name);
+    expect(employee.id).toBe(id);
+    expect(employee.email).toBe(email);
+
+})
+
+test('')
