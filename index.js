34a62f7c1ea5703344a89fed4812469e3e7d654f
LF56 <leah.fox7@gmail.com>
2022-03-02 17:55:09 -0500

    successful engineer question added

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 //node requirements
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateHTML = require('./src/generateHTML');
+// const generateHTML = require('./src/generateHTML');
 
 
 //profile for team selections
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+// const Engineer = require('./lib/Engineer');
+// const Intern = require('./lib/Intern');
+// const Manager = require('./lib/Manager');
+
+//engineer question promts
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the teams engineer?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your engineer name.');
+                  return false;
+                }
+              }
+        },
+    ])
+    
 
 
-
+// const questions = [
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is the engineer name for this team?',
+//       validate: nameInput => {
+//         if (nameInput) {
+//           return true;
+//         } else {
+//           console.log('Please enter your engineer name.');
+//           return false;
+//         }
+//       }
+//     },
+// ]
