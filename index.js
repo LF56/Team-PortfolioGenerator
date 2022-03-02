4f9d389d2cd393ea96728fff393700b48043957f
LF56 <leah.fox7@gmail.com>
2022-03-02 17:59:43 -0500

    engineers node questions successful

--- index.js
+++ index.js
@@ -25,6 +25,46 @@ inquirer
                 }
               }
         },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: 'Enter engineer ID?',
+            validate: engineerID => {
+                if (engineerID) {
+                  return true;
+                } else {
+                  console.log('Please enter your engineer ID.');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'Please enter engineer email address',
+            validate: engineerEmail => {
+                if (engineerEmail) {
+                  return true;
+                } else {
+                  console.log('Please enter your engineer email address.');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'Please enter engineer Github.',
+            validate: engineerGithub => {
+                if (engineerGithub) {
+                  return true;
+                } else {
+                  console.log('Please enter your engineer Github.');
+                  return false;
+                }
+              }
+        },
+
     ])
     
 
