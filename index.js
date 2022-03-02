2411a79e96c639b3af627600880806cf5682b891
LF56 <leah.fox7@gmail.com>
2022-03-02 17:34:22 -0500

    added node to index js

---
+++ index.js
@@ -0,0 +1,13 @@
+//node requirements
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateHTML = require('./src/generateHTML');
+
+
+//profile for team selections
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+
+
