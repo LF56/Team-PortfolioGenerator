bb9a28fb299197fb378b6308c36f024e4731d08d
LF56 <leah.fox7@gmail.com>
2022-03-02 17:10:54 -0500

    added intern test object

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,10 +1,15 @@
-const Intern = require('../lib/Intern');
-
+const Intern  = require('../lib/Intern');
 
 test('create intern object', () => {
     const name = 'Alex';
-    const id = '6780';
-    const email = 'alex@gmail.com';
-    const school = 'Community College of Lake Mary';
+    const id = '7890';
+    const email = 'Alex@gmail.com';
+    const school = 'Community School Of Heathrow';
+    
+    const intern = new Intern(name,id,email,school);
 
-    const Intern = new Intern(name, id, email, school);
+    expect(intern.name).toBe(name);
+    expect(intern.id).toBe(id);
+    expect(intern.email).toBe(email);
+    expect(intern.school).toBe(school);
+})
