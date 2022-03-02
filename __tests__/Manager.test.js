93bf499c0a42f235a324df798fc02efb68ed1309
LF56 <leah.fox7@gmail.com>
2022-03-02 17:20:45 -0500

    added manager object test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager  = require('../lib/Manager');
+
+test('create manager object', () => {
+    const name = 'Holly';
+    const id = '2222';
+    const email = 'Holly@gmail.com';
+    const officeNumber = '4073334421'
+    const manager = new Manager(name,id,email,officeNumber);
+
+    expect(manager.name).toBe(name);
+    expect(manager.id).toBe(id);
+    expect(manager.email).toBe(email);
+    expect(manager.officeNumber).toBe(officeNumber);
+  
+})
+
